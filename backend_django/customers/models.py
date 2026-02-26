7436f32ab2f004178ce2ae245f6734e479f27f18
Carlo <81carlo@gmail.com>
2026-02-26 18:44:44 +0100

    Modelo "Cliente" creado

--- backend_django/customers/models.py
+++ backend_django/customers/models.py
@@ -13,13 +13,13 @@ class Cliente(models.Model):
         return f'Cliente: {self.nombre} {self.apellido_1} {self.apellido_2}'
     
 telefono_validator = RegexValidator(
-    regex=r'^(?:\+34)?\d{9}$',
-    message='Teléfono inválido. Usa 9 dígitos (opcional +34).'
+    regex=r'^\+\d{1,3}\d{6,12}$',
+    message='Teléfono inválido. Usa formato internacional con el + delante, ej: +34600111222.'
 )
 
 class Telefono(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, related_name='telefonos')
-    telefono = models.CharField(max_length=15, validators=[telefono_validator])    
+    telefono = models.CharField(max_length=16, validators=[telefono_validator])    
     
     def __str__(self):
         return f'Cliente: {self.cliente} || Teléfono: {self.telefono}'
