4a41d9a2c9f1873286c97031b805318e67eba42a
Francisco-Gomera <225b9312@live.uem.es>
2026-02-26 15:26:00 -0400

    SERIALIZER CREADO PARA USERS

---
+++ backend_django/users/serializer.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True, required=False, min_length=4)
+
+    class Meta:
+        model = User
+        fields = ['id_usuario', 'username', 'email', 'nombre', 'password']
+        read_only_fields = ['id_usuario']
+
+    def validate_email(self, value):
+        if '@' not in value:
+            raise serializers.ValidationError("Email debe contener el simbolo '@'.")
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        if not password:
+            raise serializers.ValidationError({'password': 'Este campo es requerido.'})
+
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
