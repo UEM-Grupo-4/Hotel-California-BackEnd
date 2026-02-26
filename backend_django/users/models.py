ed5278e9079523ea88ecccee18d3baf947c10009
Francisco-Gomera <225b9312@live.uem.es>
2026-02-26 15:05:52 -0400

    EMAIL AÑADIDO A MODELO USER

--- backend_django/users/models.py
+++ backend_django/users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     nombre = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.username
