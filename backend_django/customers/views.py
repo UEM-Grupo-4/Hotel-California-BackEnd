455ce3aea18de1fb135facfd14c5bfa2430f0dcb
Francisco-Gomera <225b9312@live.uem.es>
2026-02-27 20:40:42 -0400

    VIEWS Y URLS PARA CLIENTES LISTAS, TAMBIEN EJECUTE MIGRACION A BASE DE DATOS Y REALIZE PETICIONES EN POSTMAN VERIFICANDO QUE TODO EL CRUD DE CLIENTES ES FUNCIONAL

--- backend_django/customers/views.py
+++ backend_django/customers/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .models import Cliente, Telefono
+from .serializer import (
+    ClienteDetailSerializer,
+    ClienteSerializer,
+    TelefonoDetailSerializer,
+    TelefonoSerializer,
+)
+
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = ClienteSerializer
+    lookup_field = 'id'
+    
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return ClienteDetailSerializer
+        return ClienteSerializer
+
+
+class TelefonoViewSet(viewsets.ModelViewSet):
+    queryset = Telefono.objects.select_related('cliente').all()
+    permission_classes = [AllowAny]
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return TelefonoDetailSerializer
+        return TelefonoSerializer
