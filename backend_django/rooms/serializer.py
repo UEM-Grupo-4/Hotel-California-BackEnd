0b0748ccea955b2dc49f8daf493cbfdf56c31ae2
Francisco-Gomera <225b9312@live.uem.es>
2026-03-02 18:01:45 -0400

    Habitaciones listas

--- backend_django/rooms/serializer.py
+++ backend_django/rooms/serializer.py
@@ -7,12 +7,17 @@ class TipoHabitacionSerializer(serializers.ModelSerializer):
         fields = ['id', 'tipo', 'capacidad', 'precio_noche']
 
 class HabitacionSerializer(serializers.ModelSerializer):
-    tipo = TipoHabitacionSerializer()
+    tipo = serializers.PrimaryKeyRelatedField(queryset=TipoHabitacion.objects.all())
 
     class Meta:
         model = Habitacion
         fields = ['id', 'numero', 'descripcion', 'estado', 'tipo', 'creada_el', 'actualizada_el']
-        read_only_fields = ['id', 'creada_el', 'actualizada_el']    
+        read_only_fields = ['id', 'creada_el', 'actualizada_el']
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['tipo'] = TipoHabitacionSerializer(instance.tipo).data
+        return ret    
 
 class HabitacionDetailSerializer(HabitacionSerializer):
     tipo = TipoHabitacionSerializer()
--- backend_django/rooms/views.py
+++ backend_django/rooms/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Habitacion, TipoHabitacion
+from .serializer import HabitacionSerializer, TipoHabitacionSerializer
 
 # Create your views here.
+class HabitacionViewSet(viewsets.ModelViewSet):
+    queryset = Habitacion.objects.select_related('tipo').all()
+    permission_classes = [AllowAny]
+    serializer_class = HabitacionSerializer
+    lookup_field = 'id'
+
+class TipoHabitacionViewSet(viewsets.ModelViewSet):
+    queryset = TipoHabitacion.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = TipoHabitacionSerializer
+    lookup_field = 'id'
+
+
