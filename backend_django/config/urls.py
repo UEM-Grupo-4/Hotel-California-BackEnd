ea23dfb83ecf11b9bfd3144b2fbd792207bf79ab
joaquin.pettinari <joaquin.pettinari@hotmail.com>
2026-03-05 17:56:20 +0100

    Remove comments

--- backend_django/config/urls.py
+++ backend_django/config/urls.py
@@ -22,11 +22,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # schema json
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # swagger ui
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    # redoc opcional
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 
     path('users/', include('users.urls')),
