0cbe1ba6f13319facdee8fab131626ac0e29206d
Carlo <81carlo@gmail.com>
2026-03-28 22:14:29 +0100

    Función para generar notificaciones desde reservas

---
+++ backend_django/notifications/utils.py
@@ -0,0 +1,20 @@
+from bookings.models import Reserva
+from notifications.models import Notificacion
+
+def crear_notificacion_reserva(reserva, canal=Notificacion.OpcionesCanal.EMAIL):
+    if reserva.estado == Reserva.OpcionesEstado.CONFIRMADA:
+        mensaje = f"Tu reserva con ID {reserva.id} ha sido confirmada."
+    elif reserva.estado == Reserva.OpcionesEstado.RECHAZADA:
+        mensaje = f"Tu reserva con ID {reserva.id} ha sido rechazada."
+        
+    else:
+        return None
+    
+    notificacion = Notificacion.objects.create(
+        reserva=reserva,
+        canal=canal,
+        estado=Notificacion.OpcionesEstado.PENDIENTE,
+        mensaje=mensaje,
+    )
+    
+    return notificacion
