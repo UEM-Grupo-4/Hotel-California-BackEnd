b5145c4bd0266691cceb91e960b7c026556bdf54
Carlo <81carlo@gmail.com>
2026-04-06 22:34:02 +0200

    Añadimos bearer para el deltatime del token

--- backend_django/config/settings.py
+++ backend_django/config/settings.py
@@ -73,6 +73,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
